f38d8bac720780062c6891716bd6c54ceb96cf9d
Robert Wang <glorywong1001@gmail.com>
2026-05-30 03:10:39 +0800

    fix: add brand favicons

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,16 @@ export const links: Route.LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
+  {
+    rel: 'icon',
+    href: '/favicon.ico',
+    media: '(prefers-color-scheme: light)',
+  },
+  {
+    rel: 'icon',
+    href: '/favicon-dark.ico',
+    media: '(prefers-color-scheme: dark)',
+  },
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
