5823e236090a3000811af6438e90b72dba26472f
Robert Wang <glorywong1001@gmail.com>
2026-06-01 03:42:22 +0800

    fix: update project name

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,7 +1,7 @@
 import packageJson from '../../package.json'
 
-export const APP_TITLE = 'Sindres Sorhus'
-export const APP_DESCRIPTION = 'Search Sindres Sorhus GitHub repositories for your desired libs, tools, and more.'
+export const APP_TITLE = 'Repo Lens'
+export const APP_DESCRIPTION = 'Search GitHub repos. Open any README in a structured, readable view.'
 export const APP_VERSION = packageJson.version
 export const USER_NAME = 'sindresorhus'
 export const TEXT_QUERY_KEY = 'q'
